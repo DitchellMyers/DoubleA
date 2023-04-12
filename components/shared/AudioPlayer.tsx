ef9102d33d62d17a2e5c63717bf46bbfb8d713f6
DoubleAFestival <Doublea.aalen@gmail.com>
2023-04-12 22:57:28 +0200

    test

--- components/shared/AudioPlayer.tsx
+++ components/shared/AudioPlayer.tsx
@@ -5,8 +5,7 @@ interface AudioPlayerProps {
 export const AudioPlayer = ({ src }: AudioPlayerProps) => {
   return (
     <>
-      <iframe className="hidden" allow="autoplay" src={"/silence.mp3"} id="audio" />
-      <audio autoPlay loop src={src}></audio>
+      <iframe className="hidden" allow="autoplay" src={src} />
     </>
   )
 }
