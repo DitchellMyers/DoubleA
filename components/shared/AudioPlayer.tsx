56a70c80c519b4338af8b19e1fe414ca2d5479a6
DoubleAFestival <Doublea.aalen@gmail.com>
2023-04-12 23:07:32 +0200

    maybe

--- components/shared/AudioPlayer.tsx
+++ components/shared/AudioPlayer.tsx
@@ -5,7 +5,14 @@ interface AudioPlayerProps {
 export const AudioPlayer = ({ src }: AudioPlayerProps) => {
   return (
     <>
-      <iframe className="hidden" allow="autoplay" src={src} />
+      <iframe
+        className="hidden"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ?modestbranding=1&autohide=1&controls=0&mute=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=dQw4w9WgXcQ"
+        title="YouTube video player"
+        allow="autoplay;"
+      ></iframe>
     </>
   )
 }
